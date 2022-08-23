7ea2d0303eed60e4dab66db6fbc8175f36febecf
Iakov Lilo <jlilo@hwdtech.com>
2022-08-23 17:55:07 +0600

    Add project files.

---
+++ OpenSilverNet7Pipeline.Browser/Pages/Index.cs
@@ -0,0 +1,26 @@
+﻿using DotNetForHtml5;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.JSInterop;
+using OpenSilverNet7Pipeline.Browser.Interop;
+
+namespace OpenSilverNet7Pipeline.Browser.Pages
+{
+    [Route("/")]
+    public class Index : ComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder __builder)
+        {
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Cshtml5Initializer.Initialize(new UnmarshalledJavaScriptExecutionHandler(JSRuntime));
+            Program.RunApplication();
+        }
+
+        [Inject]
+        private IJSRuntime JSRuntime { get; set; }
+    }
+}
---
+++ OpenSilverNet7Pipeline.Simulator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using OpenSilver.Simulator;
+using System;
+
+namespace OpenSilverNet7Pipeline.Simulator
+{
+    internal static class Startup
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            return SimulatorLauncher.Start(typeof(App));
+        }
+    }
+}
